16f545e3666b2faca6133ab6497647fb4d9d72e6
Roberts2393 <148310826+Roberts2393@users.noreply.github.com>
2023-10-18 08:35:20 +0300

    First version of the game

---
+++ game.py
@@ -0,0 +1,23 @@
+import random
+number = random.randint(1, 100)
+guess = 0
+tries = []
+
+while guess != number:
+    if guess < number:
+        print("Pamēģini lielāku skaitli.")
+    else:
+        print("Pamēģini mazāku skaitli.")
+
+    guess = int(input("Uzmini skaitli: "))
+    tries.append(guess)
+else:
+    print(f"You win from {len(tries)} tries!")
+    print("Here is your guessing history:")
+    print(tries)
+
+    sum_of_difference = 0
+
+    for t in tries:
+        sum_of_differece += abs(t - number)
+    print(f"Average difference was {sum_of_difference/len(tries)}")
